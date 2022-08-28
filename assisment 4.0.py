20877d7e7b62d1f34b4fdefa2f57907d33a1ec73
nehasingh00r <111220329+nehasingh00r@users.noreply.github.com>
2022-08-28 03:38:41 -1200

    Add files via upload

---
+++ assisment 4.0.py
@@ -0,0 +1,91 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:\Users\Designer Sneha\assisment 4.0.py ==============
+first number56
+first number square56
+value of z
+
+value of z
+SyntaxError: invalid syntax
+
+=============== RESTART: C:\Users\Designer Sneha\assisment 4.0.py ==============
+first number25
+first number square25
+625
+
+=============== RESTART: C:\Users\Designer Sneha\assisment 4.0.py ==============
+first number25
+first number square25
+625
+base of triangle12
+height of triangle20
+formula of triangle0.50
+120.0
+
+=============== RESTART: C:\Users\Designer Sneha\assisment 4.0.py ==============
+first number12
+2nd number13
+3rd nnumber15
+30.0
+
+=============== RESTART: C:\Users\Designer Sneha\assisment 4.0.py ==============
+ princle value200
+ rate of interest value2
+ time vale1
+Traceback (most recent call last):
+  File "C:\Users\Designer Sneha\assisment 4.0.py", line 7, in <module>
+    simple_interst=Principle*rate*Time/2
+NameError: name 'rate' is not defined. Did you mean: 'Rate'?
+
+=============== RESTART: C:\Users\Designer Sneha\assisment 4.0.py ==============
+ princle value600
+ rate of interest value2
+ time vale1
+Traceback (most recent call last):
+  File "C:\Users\Designer Sneha\assisment 4.0.py", line 8, in <module>
+    print(simple_interest)
+NameError: name 'simple_interest' is not defined. Did you mean: 'simple_interst'?
+
+=============== RESTART: C:\Users\Designer Sneha\assisment 4.0.py ==============
+ princle value500
+ rate of interest value2
+ time vale3
+simple_interest
+
+=============== RESTART: C:\Users\Designer Sneha\assisment 4.0.py ==============
+ princle value500
+ rate of interest value2
+ time vale2
+simple_interst
+
+=============== RESTART: C:\Users\Designer Sneha\assisment 4.0.py ==============
+ princle value500
+ rate of interest value2
+ time vale2
+Principle*Rate*Time
+simple_interst
+
+=============== RESTART: C:\Users\Designer Sneha\assisment 4.0.py ==============
+ princle value500
+ rate of interest value2
+ time vale3
+Principle*Rate*Time/2
+
+=============== RESTART: C:\Users\Designer Sneha\assisment 4.0.py ==============
+ princle value500
+ rate of interest value2
+ time vale3
+1500.0
+
+=============== RESTART: C:\Users\Designer Sneha\assisment 4.0.py ==============
+value of length12
+value of wide13
+value of height20
+3120
+
+=============== RESTART: C:\Users\Designer Sneha\assisment 4.0.py ==============
+value of length12
+value of wide13
+value of height15
+2340
